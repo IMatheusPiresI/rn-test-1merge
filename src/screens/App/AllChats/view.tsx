b8ac35c59a320205b5813b2033f0bf285e2bdc8a
Matheus Pires <matheuspdsousa@gmail.com>
2023-04-08 16:05:35 -0300

    fix: apply keyboardDismiss

--- src/screens/App/AllChats/view.tsx
+++ src/screens/App/AllChats/view.tsx
@@ -5,28 +5,34 @@ import { CardChatMessage } from '@components/CardChatMessage';
 import { StyleSheet } from 'react-native';
 import { IViewProps } from './types';
 import { TipCommunitySelected } from '@components/TipCommunitySelected';
+import { KeyboardDismiss } from '@components/KeyboardDismiss';
 
 const AllChatsView: React.FC<IViewProps> = ({ chats, handleOpenChat }) => (
-  <VStack flex={1}>
-    <TipCommunitySelected />
-    <VStack flex={1} bgColor="#F7F7F7" py="statusBarHeight16" px="5">
-      <Box>
-        <HeaderChat />
-      </Box>
-      <VStack flex={1} mt={4}>
-        <FlatList
-          data={chats}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <CardChatMessage data={item} onPress={() => handleOpenChat(item)} />
-          )}
-          showsVerticalScrollIndicator={false}
-          ItemSeparatorComponent={() => <Box h={4} />}
-          contentContainerStyle={styles.list}
-        />
+  <KeyboardDismiss>
+    <VStack flex={1}>
+      <TipCommunitySelected />
+      <VStack flex={1} bgColor="#F7F7F7" py="statusBarHeight16" px="5">
+        <Box>
+          <HeaderChat />
+        </Box>
+        <VStack flex={1} mt={4}>
+          <FlatList
+            data={chats}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <CardChatMessage
+                data={item}
+                onPress={() => handleOpenChat(item)}
+              />
+            )}
+            showsVerticalScrollIndicator={false}
+            ItemSeparatorComponent={() => <Box h={4} />}
+            contentContainerStyle={styles.list}
+          />
+        </VStack>
       </VStack>
     </VStack>
-  </VStack>
+  </KeyboardDismiss>
 );
 
 export default AllChatsView;
--- src/screens/App/Community/view.tsx
+++ src/screens/App/Community/view.tsx
@@ -6,32 +6,35 @@ import { StyleSheet } from 'react-native';
 import { mockCommunity } from '../../../mocks/mockCommunity';
 import { IViewProps } from './types';
 import { TipCommunitySelected } from '@components/TipCommunitySelected';
+import { KeyboardDismiss } from '@components/KeyboardDismiss';
 
 const CommunityView: React.FC<IViewProps> = ({ handleSelectCommunity }) => (
-  <VStack flex={1}>
-    <TipCommunitySelected />
-    <VStack flex={1} bgColor={'#F7F7F7'} py="statusBarHeight16" px={5}>
-      <Box h={16} w="full">
-        <InputSearch placeholder="Pesquise por comunidade..." />
-      </Box>
-      <VStack flex={1} mt={4}>
-        <FlatList
-          data={mockCommunity}
-          showsVerticalScrollIndicator={false}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <CardCommunity
-              data={item}
-              onPress={() => handleSelectCommunity(item.id, item.title)}
-            />
-          )}
-          ItemSeparatorComponent={() => <Box h={4} />}
-          contentContainerStyle={styles.list}
-          overScrollMode="never"
-        />
+  <KeyboardDismiss>
+    <VStack flex={1}>
+      <TipCommunitySelected />
+      <VStack flex={1} bgColor={'#F7F7F7'} py="statusBarHeight16" px={5}>
+        <Box h={16} w="full">
+          <InputSearch placeholder="Pesquise por comunidade..." />
+        </Box>
+        <VStack flex={1} mt={4}>
+          <FlatList
+            data={mockCommunity}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <CardCommunity
+                data={item}
+                onPress={() => handleSelectCommunity(item.id, item.title)}
+              />
+            )}
+            ItemSeparatorComponent={() => <Box h={4} />}
+            contentContainerStyle={styles.list}
+            overScrollMode="never"
+          />
+        </VStack>
       </VStack>
     </VStack>
-  </VStack>
+  </KeyboardDismiss>
 );
 
 export default CommunityView;
